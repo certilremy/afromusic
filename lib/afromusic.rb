dd4596ca4e8f9473213d133b90e5cddc679fb1c0
certilremy <certilremy@gmail.com>
2018-11-09 10:41:12 -0500

    added the music scrapping functionality and some fix to the afro music class

--- lib/afromusic.rb
+++ lib/afromusic.rb
@@ -1,7 +1,10 @@
 require_relative "./afromusic/version"
+require "open-uri"
+require "nokogiri"
 
 module Afromusic
   class Error < StandardError; end
   # Your code goes here...
 end
 require_relative './afromusic/cli'
+require_relative './afromusic/music'
